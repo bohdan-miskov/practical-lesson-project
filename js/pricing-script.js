683bf9fd95339ca68f6bb3860eccbbec21fc87c4
Bohdan Miskov <bohdanmiskov07@gmail.com>
2024-12-05 16:14:54 +0200

    Add :hover, :focus for buttons

---
+++ js/pricing-script.js
@@ -0,0 +1,24 @@
+document.addEventListener('DOMContentLoaded', () => {
+    // Знаходимо всі кнопки в елементах <li> з класом "asked-item"
+    const buttons = document.querySelectorAll('.asked-item-btn');
+    
+    buttons.forEach(button => {
+      button.addEventListener('click', () => {
+        // Отримуємо батьківський <li>, в якому знаходиться кнопка
+        const parentLi = button.closest('.asked-item');
+        const content = parentLi.querySelector('.asked-item-content-container');
+        content.style.maxHeight = 28+'px';
+        
+        if (parentLi) {
+          // Перемикаємо клас active
+          parentLi.classList.toggle('active');
+        }
+        
+        
+        if (parentLi.classList.contains('active')) {
+          content.style.maxHeight = content.scrollHeight + 'px'; // Встановлення потрібної висоти
+        } 
+      });
+    });
+  });
+  
